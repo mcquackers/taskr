443d632918c2f7a87f9ec8b21262d6195e9b4112
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-21 11:53:40 -0400

    Users can create new tasks
    Adds error messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks
+    @task = current_user.tasks.new
+    @tasks = current_user.tasks.all
+  end
+
+  def create
+    task = current_user.tasks.new(task_params)
+    if task.save
+      redirect_to tasks_path
+    else
+      @task = task
+      @tasks = current_user.tasks.all
+      render :index
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
