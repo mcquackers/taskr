c6e452817510bcd45f0d7f58068f94415d998125
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-21 11:20:11 -0400

    tasks#index now lists current_user's tasks; users have many tasks

---
+++ db/migrate/20141021145248_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :tasks, :user_id
+  end
+end
