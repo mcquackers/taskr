d788446398bb8bafe81600da193ce061fe957475
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-21 14:45:47 -0400

    Refactors completing tasks to make tasks#update reusable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
     if @task.save
-      redirect_to root_path
+      redirect_to tasks_path
     else
       @tasks = current_user.tasks.incomplete
       render :index
@@ -16,13 +16,12 @@ class TasksController < ApplicationController
 
   def update
     task = Task.find(params[:id])
-    task.completed = true
-    task.save
+    task.update(task_params)
     redirect_to tasks_path
   end
 
   private
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :completed)
   end
 end
