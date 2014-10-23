12e87f6d6944579f0d39ee6c078eb818b23e7f38
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-23 15:23:56 -0400

    Users can edit title and description in javascript, resulting in a highlight flash on completion

--- app/assets/javascripts/delete_task.js
+++ app/assets/javascripts/delete_task.js
@@ -5,12 +5,13 @@ $(function() {
     var button = $(this);
     var taskData = button.serialize();
     var convo = $.ajax({ url: button.attr("action") , type: "DELETE", data: taskData });
-    convo.done(removeTaskFromList);
+    $(this).parent("li").fadeOut();
+   // convo.done(removeTaskFromList);
     return false;
   };
 
   var removeTaskFromList = function(task) {
-    taskList.find("#task_" + task.id).fadeOut();
+    taskList.find("#task_" + task.data("id")).fadeOut();
   };
 
   //$("#completed-task-list").on("submit", ".delete", deleteDataFromServer);
---
+++ app/assets/javascripts/edit-task.js
@@ -0,0 +1,30 @@
+$(function() {
+  var makeEdit = function(){
+    $(this).attr("contenteditable", true).focus();
+  };
+  var isEnter = function(event) {
+    if(event.keyCode === 13) {
+      var updatedData = $(this);
+      updatedData.attr("contenteditable", false);
+      updateTaskData(updatedData) 
+    }
+  };
+
+  var updateTaskData = function(updatedData) {
+    var key = updatedData.attr("class");
+    var data = {};
+    data[key] = updatedData.text();
+    debugger
+    var convo = $.ajax({ 
+      url: "/tasks/"+updatedData.parent().data("id"), 
+      type: "PATCH", 
+      data: {task: data}
+    });
+    convo.done(updatedData.highlight());
+    return false; 
+  };
+
+
+  $(document).on("keydown", ".title, .description", isEnter);
+  $("body").on("click", ".title, .description", makeEdit);
+});
